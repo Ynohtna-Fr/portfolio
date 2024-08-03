a19eb70046eeb3ec44d4a359684d72248eeca520
Ynohtna-Fr <aazstudio01@gmail.com>
2024-08-03 19:26:12 +0200

    j'ai fini

--- js/app.js
+++ js/app.js
@@ -1,1 +1,16 @@
-require('fslightbox')
+const comedianImg = document.querySelector('#comedian-img')
+const mesImg = document.querySelector('#mes-img')
+
+document.querySelector('#comedian').addEventListener('mouseover', (e) => {
+  if (mesImg.classList.contains('active')) {
+    mesImg.classList.remove('active')
+  }
+  comedianImg.classList.add('active')
+})
+document.querySelector('#mes').addEventListener('mouseover', (e) => {
+  if (comedianImg.classList.contains('active')) {
+    comedianImg.classList.remove('active')
+  }
+  mesImg.classList.add('active')
+})
+
